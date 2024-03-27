9c12ebbc77eef822c4ed5fb60406cf11b4d67e62
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-03-27 20:31:47 +0000

    Add NavBar to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Button from 'react-bootstrap/Button';
-import './App.css';
+import NavBar from './components/NavBar';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <Button variant="primary">Primary</Button>
+    <div className={styles.App}>
+      <NavBar/>
     </div>
   );
 }
