b41f11f8db1b5e859fe32360f9e3b9aad38f49a9
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-03-29 05:41:00 +0000

    Update App.js and NavBar.js with currentuser

--- src/api/axiosDefault.js
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-axios.defaults.baseURL = 'https://seecrets-d488911dd03c.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
