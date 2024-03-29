8a40013c3e126c0ed36c0859a0e64dc8da71cca4
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-03-29 05:39:10 +0000

    Fix typo in axiosDefaults file

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://seecrets-d488911dd03c.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
