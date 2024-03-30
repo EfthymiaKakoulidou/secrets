568c9a76ed64847e30d0912be00b40df5b3be1bf
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-03-30 15:38:58 +0000

    Add PostPage Post.js and styles

---
+++ src/pages/posts/PostPage.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
+
+function PostPage() {
+    const { id } = useParams();
+    const [post, setPost] = useState({ results: [] });
+
+    useEffect(() => {
+        const handleMount = async () => {
+          try {
+            const [{ data: post }] = await Promise.all([
+              axiosReq.get(`/seecrets/${id}`),
+            ]);
+            setPost({ results: [post] });
+            console.log(post);
+          } catch (err) {
+            console.log(err);
+          }
+        };
+    
+        handleMount();
+      }, [id]);
+    
+
+  return (
+    <Row className="h-100">
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+      <Post {...post.results[0]} setPosts={setPost} postPage />
+        <p>Post component</p>
+        <Container className={appStyles.Content}>
+          Comments
+        </Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        Profiles
+      </Col>
+    </Row>
+  );
+}
+
+export default PostPage;
