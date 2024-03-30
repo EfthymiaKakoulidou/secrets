933e9e9dd063815c3af87d13902733e4a1b3a658
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-03-30 08:32:36 +0000

    Add PostCreateForm and Asset.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -25,7 +25,7 @@ const NavBar = () => {
 
   const addPostIcon = (
       <NavLink
-        to="/seecrets/create"
+        to="/secrets/create"
         className={styles.NavLink}
         activeClassName={styles.Active}
       >
