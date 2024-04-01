c87cdf86836f436d71aa3d4057815e8b569c07d0
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-04-01 07:27:47 +0000

    Add route for feed and hugs

--- src/App.js
+++ src/App.js
@@ -7,16 +7,23 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || '';
+
   return (
 
         <div className={styles.App}>
           <NavBar />
           <Container className={styles.Main}>
             <Switch>
-              <Route exact path="/" render={() => <h1>Home page</h1>} />
+              <Route exact path="/" render= {() => ( <PostsPage message='No results are found.Adjust the search keyword' /> )}/>
+              <Route exact path="/feed" render= {() => ( <PostsPage message='No results are found.Adjust the search keyword' /> )}/>
+              <Route exact path="/hugs" render= {() => ( <PostsPage message='No results are found.Adjust the search keyword or hug somebody' filter={`hugs__owner__profile=${profile_id}&ordering=-hugs__created_at&`}/> )}/>
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
               <Route exact path="/seecrets/create" render={() => <PostCreateForm />} />
