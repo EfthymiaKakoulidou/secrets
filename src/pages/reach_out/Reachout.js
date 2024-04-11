81ebce65dc4fde41edf333166f87afb03de69492
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-04-11 06:52:15 +0000

    Fetch reach_out_to's name from the profiles/id url

--- src/pages/reach_out/Reachout.js
+++ src/pages/reach_out/Reachout.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
@@ -16,6 +16,7 @@ const Reach_out = (props) => {
     updated_at,
     content,
     id,
+    reach_out_to,
     setProfile,
     setReach_outs,
   } = props;
@@ -24,7 +25,19 @@ const Reach_out = (props) => {
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+  const [reachOutToName, setReachOutToName] = useState("");
 
+  useEffect(() => {
+    const fetchProfileName = async () => {
+      try {
+        const response = await axiosRes.get(`/profiles/${reach_out_to}`);
+        setReachOutToName(response.data.owner);
+      } catch (error) {
+        console.error("Error fetching profile name:", error);
+      }
+    };
+    fetchProfileName();
+  }, [reach_out_to]);
 
   return (
     <>
@@ -35,7 +48,8 @@ const Reach_out = (props) => {
         </Link>
         <Link to={`/reach_out/${id}`}>
         <Media.Body className="align-self-center ml-2">
-          <span className={styles.Owner}>{owner}</span>
+          <span className={styles.Owner}>{owner} reached out to</span>
+          <span className={styles.Owner}>{reachOutToName}</span>
           <span className={styles.Date}>{updated_at}</span>
           <span className={styles.Date}>{content}</span>
         </Media.Body>
