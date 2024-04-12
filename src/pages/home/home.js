caadf3059d4ab8c0b21fc6da50d1ad7d5add7087
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-04-12 11:38:05 +0000

    Import and apply the home styles

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../../styles/Post.module.css";
+import styles from "../../styles/Home.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card } from "react-bootstrap";
 import Col from "react-bootstrap/Col";
@@ -12,15 +12,14 @@ const Home = () => {
     
     return (
       <Row className="h-100 ">
-          <Col className="py-2 p-0 p-lg-2" lg={6}>
-          <p className="p-4">Home</p>
-      <Card className={styles.Post}>
-        <Card.Body>
-        Secrets is a secure and confidential platform designed to provide users with a safe space to share their secrets, write personal diaries, and engage in private messaging and comments. The app aims to offer emotional support and relief by allowing users to unburden themselves while also fostering connections with others going through similar experiences
-        </Card.Body>
-        
-        
-      </Card></Col>
+        <Col className="py-2 p-0 p-lg-2" lg={6}>
+          <h4 className="p-4">Secrets</h4>
+            <Card className={styles.Home}>
+              <Card.Body>
+              Secrets is a secure and confidential platform designed to provide users with a safe space to share their secrets, write personal diaries, and engage in private messaging and comments. The app aims to offer emotional support and relief by allowing users to unburden themselves while also fostering connections with others going through similar experiences
+              </Card.Body>
+            </Card>
+        </Col>
       </Row>
     );
   };
