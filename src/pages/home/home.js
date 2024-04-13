bd83d228686eaf02403acdc60d1cf9eb92b3743d
EfthymiaKakoulidou Kakoulidou <efthymiakakoulidou@gmail.com>
2024-04-13 06:37:00 +0000

    Style home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -13,9 +13,9 @@ const Home = () => {
     return (
       <Row className="h-100 ">
         <Col className="py-2 p-0 p-lg-2" lg={6}>
-          <h4 className="p-4">Secrets</h4>
+          <h4 className="p-4 text-center">Secrets</h4>
             <Card className={styles.Home}>
-              <Card.Body>
+              <Card.Body className="p-4">
               Secrets is a secure and confidential platform designed to provide users with a safe space to share their secrets, write personal diaries, and engage in private messaging and comments. The app aims to offer emotional support and relief by allowing users to unburden themselves while also fostering connections with others going through similar experiences
               </Card.Body>
             </Card>
